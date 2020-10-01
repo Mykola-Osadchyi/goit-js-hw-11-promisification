42d8c51eb1d126bd30b1cd27cc8196d5f41b7610
Mykola Osadchyi <osadchiy_n@ukr.net>
2020-10-01 22:43:44 +0300

    did mentor's remark

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './styles.css';
 import './js/task-01';
 import './js/task-02';
-import './js/task-02-second-version';
 import './js/task-03';
 
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -13,13 +13,11 @@ const users = [
 ];
 
 const toggleUserState = (allUsers, userName) => {
-  return new Promise (resolve => {
-    setTimeout(() => {
-      const updatedUsers = allUsers.map(user =>
-        user.name === userName ? { ...user, active: !user.active } : user,
-      );
-      resolve(updatedUsers);
-    }, 500);
+  return new Promise(resolve => {
+    const updatedUsers = allUsers.map(user =>
+      user.name === userName ? { ...user, active: !user.active } : user,
+    );
+    resolve(updatedUsers);
   });
 };
 
